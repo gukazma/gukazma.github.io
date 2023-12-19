ffe378c960dfa36113ecfd94535513650edc1d06
gukazma <gukazma@proton.me>
2023-12-19 13:40:22 +0800

    ✨ feat: layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
-    <main>
+    <Layout pageTitle="About Me">
       <h1>About Me</h1>
       <Link to="/">Back to Home</Link>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    </Layout>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 // Step 1: Import React
 import * as React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
+    <Layout pageTitle="Home Page">
       <h1>Welcome to my Gatsby site!</h1>
       <Link to="/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    </Layout>
   )
 }
 
